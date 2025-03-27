9e4a6855079f44d4f02cc7cf0b00ad2c18d4469b
Harry Hurst <harryhurst@Harrys-MacBook-Pro.local>
2025-03-27 09:46:25 +0000

    add static searchResults object in page.js

--- app/components/searchResults.js
+++ app/components/searchResults.js
@@ -1,3 +1,22 @@
-export default function SearchResults() {
-  return <></>;
+import styles from "./searchResults.module.css";
+
+export default function SearchResults({ searchResults }) {
+  return (
+    <>
+      <div>
+      
+          {searchResults.map((result, index) => (
+
+              <div key={index} className={styles.resultItem}>
+                <div>{index + 1}</div>
+                <div>{result.song}</div>
+                <div>{result.artist}</div>
+                <div>{result.album}</div>
+              </div>
+            
+          ))}
+        
+      </div>
+    </>
+  );
 }
--- app/page.js
+++ app/page.js
@@ -1,18 +1,35 @@
+import styles from "./page.module.css"
+
 import SearchBar from "./components/searchBar";
 import SearchResults from "./components/searchResults";
 import Playlist from "./components/playlist";
 import Tracklist from "./components/tracklist";
 import Track from "./components/track";
 
+const searchResults = [{
+  artist: "Drake",
+  song: "Headlines",
+  album: "Take Care (Delux)"
+},
+{
+  artist: "AJ Tracy, Jorja Smith",
+  song: "Crush (feat. Jorja Smoth)",
+  album: ""
+},
+{
+  artist: "Dave, Central Cee",
+  song: "Sprinter",
+  album: ""
+}]
 
 export default function Home() {
   return (
-    <>
+    <div id={styles.containerDiv}>
       <SearchBar/>
-      <SearchResults/>
+      <SearchResults searchResults={searchResults}/>
       <Playlist/>
       <Tracklist/>
       <Track/>
-    </>
+    </div>
   );
 }
